18f46d6cac1ab64d0eed5e4a60e4e7b65ee7fb14
yg-yash <yg123yash@gmail.com>
2020-06-13 04:43:39 -0700

    Updates

---
+++ precache-manifest.a2de6ff4b8753d314b3df9924c4c01d3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b681f34c6f8eaf0cd25a9f366f762033",
+    "url": "/movieinfo/index.html"
+  },
+  {
+    "revision": "9c9e6a63954c937f6a9a",
+    "url": "/movieinfo/static/js/2.3f6c5dfe.chunk.js"
+  },
+  {
+    "revision": "84a2a7a390df1ac99fa4fadf23155e60",
+    "url": "/movieinfo/static/js/2.3f6c5dfe.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b5a0481b49c2c9b842eb",
+    "url": "/movieinfo/static/js/main.f0a858c8.chunk.js"
+  },
+  {
+    "revision": "67cead3809ec74ca5c8f",
+    "url": "/movieinfo/static/js/runtime-main.9cffcba3.js"
+  }
+]);
